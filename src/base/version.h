931f311b3bf162f6558cf3495c9d64424e01c4ea
Hang Lyu <hlyu@b01.clsp.jhu.edu>
2017-08-02 02:42:47 -0400

    small fix

--- src/base/version.h
+++ src/base/version.h
@@ -1,4 +1,4 @@
 // This file was automatically created by ./get_version.sh.
 // It is only included by ./kaldi-error.cc.
-#define KALDI_VERSION "5.1.1~39-4685"
-#define KALDI_GIT_HEAD "468563f3b01097350b3c9a7096cb931de25bdcbd"
+#define KALDI_VERSION "5.1.2~4-4b30"
+#define KALDI_GIT_HEAD "4b308ded49ccffc769e258bab7903e8df287b6fa"
